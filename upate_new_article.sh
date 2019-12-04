641f1338bee6b796ec8d73ac80718919b2eefcc9
bili123 <lang@gizlog.de>
2019-12-04 06:14:00 +0100

    tools/docker hinzugefügt

--- upate_new_article.sh
+++ upate_new_article.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 notepad readme.md && echo Weiter ...
-echo "<a href=/"$1">"$1"</a><br>"}' >> browser.html
+sed "/<\/body>/i <a href=\/$(basename $1)>$(basename $1)<\/a><br>" browser.html
 ./ml_term-to-web $1 > html/$1.html
 sed -i "/\## Ende.*/i $(basename $1)" meta/{list,liste,befehle,kommandos,programme}
 git add . 
