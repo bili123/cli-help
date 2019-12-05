894a3120eed70cb08f9a61f24ea73089c9865d29
bili123 <lang@gizlog.de>
2019-12-05 02:56:54 +0100

    Docker- und Git-HTMLs; Skript-Update

--- upate_new_article.sh
+++ upate_new_article.sh
@@ -2,7 +2,7 @@
 
 notepad readme.md && echo Weiter ...
 sed -i "/<\/body>/i <a href=\/$(basename $1)>$(basename $1)<\/a><br>" browser.html
-./ml_term-to-web $1 > html/$1.html
+./ml_term-to-web $1 > html/$(basename $1).html
 sed -i "/\## Ende.*/i $(basename $1)" meta/{list,liste,befehle,kommandos,programme}
 git add . 
 git commit -am "$1 hinzugefügt"
