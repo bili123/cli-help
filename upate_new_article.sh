c1aeb13b342ddcf7fe0217758cc964c396ae6038
bili123 <lang@gizlog.de>
2019-12-05 03:06:40 +0100

    Internes Skript und Dumps aktualisiert.

--- upate_new_article.sh
+++ upate_new_article.sh
@@ -2,7 +2,8 @@
 
 notepad readme.md && echo Weiter ...
 sed -i "/<\/body>/i <a href=\/$(basename $1)>$(basename $1)<\/a><br>" browser.html
-./ml_term-to-web $1 > html/$(basename $1).html
 sed -i "/\## Ende.*/i $(basename $1)" meta/{list,liste,befehle,kommandos,programme}
+./ml_term-to-web $1 > html/$(basename $1).html
+cat $1 | tee -a meta/{alles,dump,komplett}
 git add . 
-git commit -am "$1 hinzugefügt"
+git commit -am "$1 hinzugefügt; Listen, Dumps und readme.md aktualisiert"
