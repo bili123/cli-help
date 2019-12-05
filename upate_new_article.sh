e0bca4a1d096df73695aae4db4b65fd3edc3f4cb
bili123 <lang@gizlog.de>
2019-12-05 02:42:32 +0100

    Intern: Hilfsskript

--- upate_new_article.sh
+++ upate_new_article.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 notepad readme.md && echo Weiter ...
-sed "/<\/body>/i <a href=\/$(basename $1)>$(basename $1)<\/a><br>" browser.html
+sed -i "/<\/body>/i <a href=\/$(basename $1)>$(basename $1)<\/a><br>" browser.html
 ./ml_term-to-web $1 > html/$1.html
 sed -i "/\## Ende.*/i $(basename $1)" meta/{list,liste,befehle,kommandos,programme}
 git add . 
 git commit -am "$1 hinzugefügt"
-sftp open-source-elearning@open-source-elearning.de
